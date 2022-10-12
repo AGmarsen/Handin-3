998de51ff6bcd30479b0ea1487f3ce0a6d57e927
tcla <tcla@itu.dk>
2022-10-12 14:34:39 +0200

    setup with server proto client

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AGmarsen/Handin-3
+
+go 1.19
---
+++ server/server.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+
+	// this has to be the same as the go.mod module,
+	// followed by the path to the folder the proto file is in.
+	gRPC "github.com/AGmarsen/Handin-3/proto"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	gRPC.UnimplementedTemplateServer        // You need this line if you have a server
+	name                                    string // Not required but useful if you want to name your server
+	port                                    string // Not required but useful if your server needs to know what port it's listening to
+
+	mutex          sync.Mutex // used to lock the server to avoid race conditions.
+}
+
+// flags are used to get arguments from the terminal. Flags take a value, a default value and a description of the flag.
+// to use a flag then just add it as an argument when running the program.
+var serverName = flag.String("name", "default", "Senders name") // set with "-name <name>" in terminal
+var port = flag.String("port", "5400", "Server port")           // set with "-port <port>" in terminal
+
+func main() {
+
+	// setLog() //uncomment this line to log to a log.txt file instead of the console
+
+	// This parses the flags and sets the correct/given corresponding values.
+	flag.Parse()
+	fmt.Println(".:server is starting:.")
+
+	// starts a goroutine executing the launchServer method.
+	go launchServer()
+
+	// This makes sure that the main method is "kept alive"/keeps running
+	for {
+		time.Sleep(time.Second * 5)
+	}
+}
+
+func launchServer() {
+	log.Printf("Server %s: Attempts to create listener on port %s\n", *serverName, *port)
+
+	// Create listener tcp on given port or default port 5400
+	list, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", *port))
+	if err != nil {
+		log.Printf("Server %s: Failed to listen on port %s: %v", *serverName, *port, err) //If it fails to listen on the port, run launchServer method again with the next value/port in ports array
+		return
+	}
+
+	// makes gRPC server using the options
+	// you can add options here if you want or remove the options part entirely
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+
+	// makes a new server instance using the name and port from the flags.
+	server := &Server{
+		name:           *serverName,
+		port:           *port,
+	}
+
+	gRPC.RegisterTemplateServer(grpcServer, server) //Registers the server to the gRPC server.
+
+	log.Printf("Server %s: Listening on port %s\n", *serverName, *port)
+
+	if err := grpcServer.Serve(list); err != nil {
+		log.Fatalf("failed to serve %v", err)
+	}
+	// code here is unreachable because grpcServer.Serve occupies the current thread.
+}
+
+// The method format can be found in the pb.go file. If the format is wrong, the server type will give an error.
+func (s *Server) RequestTime(ctx context.Context, actualTime *gRPC.TimeRequest) (*gRPC.ChristianTime, error) {
+	// locks the server ensuring no one else can request the value at the same time.
+	// and unlocks the server when the method is done.
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	
+	
+	return &gRPC.ChristianTime{ActualTime: time.Now().Local().UnixMilli(), Variation: 0}, nil
+}
+
+// sets the logger to use a log.txt file instead of the console
+func setLog() {
+	// Clears the log.txt file when a new server is started
+	if err := os.Truncate("log.txt", 0); err != nil {
+		log.Printf("Failed to truncate: %v", err)
+	}
+
+	// This connects to the log file/changes the output of the log informaiton to the log.txt file.
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+}
